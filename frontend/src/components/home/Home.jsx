8ce6c246905361b9364bec4d553918e9835459f8
Somil Kwatra <115617066+somilkwatra@users.noreply.github.com>
2024-04-15 19:47:48 +0530

    Home.jsx

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         {/* Heading */}
         <h1 className='text-center'>Organize your <br />Work and Life, finally</h1>
         {/* Subheading */}
-        <p>Here is The Todo List App created by <b>Somil Kwatra</b></p>
+        <p>Here is The Todo List created by <b>Somil Kwatra</b></p>
         {/* Button to make a Todo List */}
         {/* Use Link component to navigate to the signup page */}
         <Link to="/signup" className='home-btn'>Make Todo List</Link>
