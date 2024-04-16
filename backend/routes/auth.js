799846eb5953e6188bdf4e7fa0287966e8c8c685
somilkwatra <somilkwatra21@gmail.com>
2024-04-16 10:50:05 +0530

    Backend Repo

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -40,7 +40,7 @@ router.post("/register", async (req, res) => {
     }
 });
 
-// Sign-in functionality
+// Sign-in functionalit
 router.post("/signin", async (req, res) => {
     try {
         const { email, password } = req.body;
